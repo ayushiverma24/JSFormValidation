a2c08cab71549a13b487e39f88376637e8edf620
ayushiverma24 <vayushi121@gmail.com>
2022-10-06 09:21:38 +0530

    Error message change

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-document.title = "Ayushi"
 const form  = document.getElementById('form');
 const username = document.getElementById('username') ;
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const confirmPassword = document.getElementById('Cpassword');
-
+const confirmpassword = document.getElementById('confirmpassword');
+const errorMsg = {
+    'username': 'Please enter Username',
+    'email':'Please enter a valid Email Id',
+    'password':'Please enter Password',
+    'confirmpassword':'Enter your password again'
+}
 function showError(input, message){
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
+}
+const checkRequired=(inputArr)=>{
+    inputArr.forEach((input)=>{
+    const errorMessage  = errorMsg[input.id];
+        if(input.value.trim() === ''){
+            showError(input,errorMessage)
+        }
+    })
 }
 const isValidEmail=(email)=>{
  const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -18,12 +30,6 @@ const isValidEmail=(email)=>{
 form.addEventListener('click',(e)=>{
     e.preventDefault();
 console.log('form submit cicked');
-if(username.value ==='') showError(username,"Please enter a username");
-if(email.value ==='') showError(email,"Please enter a email");
-else if(!isValidEmail(email.value)){
-    showError(email,"Enter a valid email");
-}
-if(password.value ==='') showError(password,"Please enter a password");
-if(Cpassword.value ==='') showError(Cpassword,"Enter your password again");
+checkRequired([username,email,password,confirmpassword])
 
 })
